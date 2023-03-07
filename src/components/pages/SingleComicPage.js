d8ab35acdf5eff0eeea6892e9810fad500e02c1c
Denis <d.khokhloff2011@yandex.ru>
2023-03-07 10:14:01 +0300

    the react-helmet library has been applied to the components

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
+import AppBanner from '../appBanner/AppBanner'; 
 
 import './singleComicPage.scss';
 
@@ -48,17 +50,27 @@ const View = ({comic}) => {
     const {thumbnail, title, description, pageCount, language, price} = comic;
     
     return (
-        <div className="single-comic">
-            <img src={thumbnail} alt={title} className="single-comic__img"/>
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">{title}</h2>
-                <p className="single-comic__descr">{description}</p>
-                <p className="single-comic__descr">{pageCount}</p>
-                <p className="single-comic__descr">Language: {language}</p>
-                <div className="single-comic__price">{price}</div>
+        <>
+            <AppBanner />
+            <div className="single-comic">
+                <Helmet>
+                    <meta
+                        name="description"
+                        content={`${title} comics book`}
+                        />
+                    <title>{title}</title>
+                </Helmet>
+                <img src={thumbnail} alt={title} className="single-comic__img"/>
+                <div className="single-comic__info">
+                    <h2 className="single-comic__name">{title}</h2>
+                    <p className="single-comic__descr">{description}</p>
+                    <p className="single-comic__descr">{pageCount}</p>
+                    <p className="single-comic__descr">Language: {language}</p>
+                    <div className="single-comic__price">{price}</div>
+                </div>
+                <Link to="/comics" className="single-comic__back">Back to all</Link>
             </div>
-            <Link to="/comics" className="single-comic__back">Back to all</Link>
-        </div>
+        </>
     );
 };
 
